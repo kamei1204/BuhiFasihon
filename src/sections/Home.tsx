73d95ff5a2bdb835025d14d909fed27dc2e6f6e2
kameiryousuke <kamei.r@i.softbank.jp>
2022-04-08 00:46:51 +0900

    second

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,16 +1,19 @@
 import styled from "styled-components"
+import Logo from "../components/Logo"
 import SunnyVideo from "../components/SunnyVideo"
 
 const Section = styled.section`
     position: relative;
     min-height: 300vh;
     overflow: hidden;
+    color: white;
 `
 
 const Home = () => {
     return (
         <Section>
             <SunnyVideo />
+            <Logo />
         </Section>
     )
 }
